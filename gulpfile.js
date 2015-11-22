8125edd2cb85457b6545c525b55a8db7035d4a75
Miguel A. Pedregosa <miguelpedregosa@gmail.com>
2015-11-22 12:39:27 +0100

    Use Nunjucks to generate pages

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,13 @@
 /**
  * Created by migue on 22/11/15.
  */
+
+var gulp = require('gulp');
+var nunjucksRender = require('gulp-nunjucks-render');
+
+gulp.task('default', function () {
+    nunjucksRender.nunjucks.configure(['./'], {watch: false});
+    return gulp.src('pages/*.html')
+        .pipe(nunjucksRender())
+        .pipe(gulp.dest('.'));
+});
